18a55c81d91ebc4d6b2f035d36610c8bba3aba84
LauWyeRock <97499447+LauWyeRock@users.noreply.github.com>
2022-05-25 13:29:44 +0800

    Color test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'darkbrown': '#8d7b72',
+      'mildbrown': '#a4978b',
+      'brown': '#c6b1a1',
+      'pinkbrown': '#dbc8b9',
+      'beige': '#e5ded2',
+      'offwhite': '#f7efe9',
+      }
+    },
   },
   plugins: [require("@tailwindcss/forms"),
   require("tailwind-scrollbar"),
